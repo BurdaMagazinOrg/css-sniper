230c524433e4b7fe0ab98ad5593a6c2e2f472871
Volker Killesreiter <killesre@gmail.com>
2017-11-24 14:57:09 +0100

    add options, file globber, directory check and run

--- index.js
+++ index.js
@@ -1,34 +1,49 @@
 #!/usr/bin/env node
 
+const fs = require('fs');
+const glob = require('glob')
+const mkdirp = require('mkdirp');
 const path = require('path');
 const sass = require('node-sass');
-// const glob = require('glob');
 const { execSync } = require('child_process');
 const { sniperImporter, sniperConfigure } = require('./importer');
-
 const program = require('commander');
 
 program
   .version(require('./package.json').version)
-  .usage('[options] <file ...>')
-  .option('--origin [value]', 'Path for origin files')
+  .usage('[options] <files ...>')
   .option('--include-path <value>', 'Paths to look for imported files ', val => val.split(','), [])
+  .option('--origin [value]', 'Path to origin theme css files', execSync('drush eval "echo DRUPAL_ROOT . \'/\'. drupal_get_path(\'theme\', \'seven\');"'))
+  .option('-o, --output [value]', 'Output directory')
+  .option('--output-style [value]', 'Available output formats: nested, expanded, compact, compressed', 'nested' )
   .parse(process.argv)
 
-program.origin = program.origin || execSync('drush eval "echo DRUPAL_ROOT . \'/\'. drupal_get_path(\'theme\', \'seven\');"');
+if (!program.args.length) program.help();
 
 program.includePath = program.includePath.length ? program.includePath : ['sass-includes'];
 let includePaths = program.includePath.map(include => path.resolve(process.cwd()+'/'+include));
-let file = program.args[0] || 'sass/base/elements.scss';
+let files = program.args[0];
 
-function fileRenderer(file) {
+// Configure importer.
+sniperConfigure(program.origin);
+
+// Render files.
+run(files);
+
+/**
+ * Render File
+ *
+ * @param {String} file
+ */
+function renderFile(file) {
   'use strict';
 
   sass.render({
     file: file,
     // data: '@import "./sass/global-styling.scss";',
     includePaths: includePaths,
-    importer: sniperImporter()
+    outputStyle: program.outputStyle,
+    importer: sniperImporter(),
   }, function (err, result) {
     if (err) {
       console.log(err.status); // used to be "code" in v2x and below
@@ -36,14 +51,63 @@ function fileRenderer(file) {
       console.log(err.message);
       console.log(err.line);
     } else {
-      // console.log(result.css.toString());
-      // console.log(result.stats);
-      // console.log(result.map.toString());
+      // Build path to destination file.
+      let dest = path.resolve(
+        path.join(program.output,
+          path.relative(
+            path.resolve(files), file)
+          )
+        ).replace(path.extname(file), '.css');
+      // Create destination dir.
+      mkdirp(path.dirname(dest), function (err) {
+        if (err) {
+          return console.log(err);
+        }
+        fs.writeFile(dest, result.css.toString(), function (err) {
+          if (err) {
+            return console.log(err);
+          }
+          console.log(dest+' written.');
+        });
+      });
     }
   });
+
 }
 
-sniperConfigure(program.origin);
-//resolveSevenDirectory();
-//fileRenderer(path.resolve(process.cwd(), '../../sass/base/elements.scss'));
-fileRenderer(path.resolve(process.cwd()+'/'+file));
+/**
+ * Is a Directory
+ *
+ * @param {String} filePath
+ * @returns {Boolean}
+ * @api private
+ */
+function isDirectory(filePath) {
+  var isDir = false;
+  try {
+    var absolutePath = path.resolve(filePath);
+    isDir = fs.statSync(absolutePath).isDirectory();
+  } catch (e) {
+    isDir = e.code === 'ENOENT';
+  }
+  return isDir;
+}
+
+/**
+ * Glob sass/scss files and render.
+ *
+ * @param {String} files
+ */
+function run(files) {
+  'use strict';
+  if (isDirectory(files)) {
+    let globPath = path.resolve(files, '**/*.{sass,scss}');
+    glob(globPath, { ignore: '**/_*', follow: true }, function (err, files) {
+      for (let file of files) {
+        renderFile(file);
+      }
+    });
+  } else {
+    renderFile(path.resolve(files));
+  }
+}
