9c0cd76375bc4b5daf1617d4a7b583689a2dfead
Volker Killesreiter <killesre@gmail.com>
2017-11-24 10:57:02 +0100

    Added some comments

--- importer.js
+++ importer.js
@@ -21,39 +21,47 @@ function sniperImporter() {
 
 }
 
+/**
+ * Parse an import string.
+ *
+ * Syntax: @import "@seven/css/base/elements.css remove { body,
+ * .thunder-details, .apple { color } }";
+ * 
+ */
 function parseImportString(string) {
   let [file, definitionString] = string.split(' remove ').map(val => val.trim());
   let selectorMatches = definitionString.match(/{([\s\S]+)}/);
-  if (selectorMatches) {
 
+  if (selectorMatches) {
     let definition = selectorMatches[1].split(/,(?![^{]*})/).map(function (string) {
 
       let [selector, declarations] = string.split(/[{}]/);
       selector = selector.trim();
+
       if (declarations) {
         declarations = declarations.split(',').map(val => val.trim());
       }
       else {
         declarations = [];
       }
+
       return { selector: selector, declarations: declarations };
     });
 
     return [ definition, file ];
   }
+
   return [ null, file];
 }
 
 
 /**
- * Usage: @import "@seven/css/base/elements.css remove { body,
- * .thunder-details, .apple: [color] }";
  *
  */
 function parseFile(file, definitions){
   let contents = fs.readFileSync(file, 'utf-8');
 
-  // https://astexplorer.net
+  // Parse css into ast, see https://astexplorer.net
   let ast = csstree.parse(contents, {
     filename: file,
     tolerant: true,
@@ -62,21 +70,28 @@ function parseFile(file, definitions){
     }
   });
 
+  // Use walkRulesRight: added rules are not parsed.
   csstree.walkRulesRight(ast, function(rule, item, list) {
-
+    // Ignore all other types.
     if (rule.type !== 'Rule') {
       return;
     }
-    let remove = {};
+
+    let reduce = {};
+    // Iterate selectors in rule prelude.
     rule.prelude.children.each(function (node, item, list) {
+
+      // Render selector from ast for comparison.
       let name = csstree.translate(item.data);
       let definition = definitions.find(function (def) {
         return def.selector === name;
       });
+
       if (definition) {
         if (definition.declarations.length) {
-          remove.selector = list.remove(item);
-          remove.declarations = definition.declarations;
+          // Remove the selector from the rule, a new rule is created below.
+          reduce.selector = list.remove(item);
+          reduce.declarations = definition.declarations;
         }
         else {
           list.remove(item);
@@ -85,24 +100,26 @@ function parseFile(file, definitions){
     });
 
     // Handle removal of declarations by creating a new rule.
-    if (remove.selector) {
+    if (reduce.selector) {
       let block = new csstree.List()
+      // Copy declarations which are not removed.
       rule.block.children.each(function(node, item, list) {
         if (node.type === 'Declaration') {
-          if (!remove.declarations.includes(node.property)) {
+          if (!reduce.declarations.includes(node.property)) {
             block.insertData(node);
           }
         }
       });
 
       if(!block.isEmpty()) {
+        // Build a new rule for inserting.
         let newRule = {
           type: 'Rule',
-          loc: null,    // not required, but better keep the shape of nodes
+          loc: null,
           prelude: {
             loc: null,
             type: 'SelectorList',
-            children: new csstree.List().append(remove.selector)
+            children: new csstree.List().append(reduce.selector)
           },
           block: {
             type: 'Block',
@@ -113,7 +130,7 @@ function parseFile(file, definitions){
         list.insertData(newRule, item.next);
       }
     }
-
+    // Remove rule if there is no selector or declaration.
     if (rule.prelude.children.isEmpty() ||
       rule.block.children.isEmpty()) {
       list.remove(item);
